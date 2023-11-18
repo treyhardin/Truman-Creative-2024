e2e0c604d69aaeae2a11cbf7f3926f2aac6b6510
Trey Hardin <treyhardindesign@gmail.com>
2023-11-18 12:31:51 -0500

    Responsiveness

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -9,31 +9,9 @@ export default function Projects() {
   let scrollContentRef
   let projectsTrack
 
-  const getScrollPercent = () => {
-
-    var h = projectsRef, 
-    b = scrollContentRef,
-    st = 'scrollTop',
-    sh = 'scrollHeight';
-
-    const percent = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
-    return percent
-  }
-
-  onMount(() => {
-
-
-    projectsRef.addEventListener("scroll", (e) => {
-      // console.log(e)
-      projectsTrack.style.translate = `-${getScrollPercent()}% 0`
-      console.log(getScrollPercent())
-    })
-  })
-
   return (
     <section class={styles.projects} ref={projectsRef}>
       <div class={styles.projectsContainer} ref={scrollContentRef}>
-        <div class={styles.projectsTrack} ref={projectsTrack}>
           <For each={data()}>{(project, i) => 
             <ProjectCard 
               title={project.title}
@@ -45,7 +23,6 @@ export default function Projects() {
             />
           }
           </For>
-        </div>
       </div>
     </section>
   )
