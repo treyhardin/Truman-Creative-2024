f595100fb66c08b7229c58c15ae95e6e6b495af5
Trey Hardin <treyhardindesign@gmail.com>
2023-11-19 12:49:58 -0500

    Basic Preloader

--- src/components/preloader/preloader.jsx
+++ src/components/preloader/preloader.jsx
@@ -3,7 +3,7 @@ import styles from './preloader.module.css'
 export default function Preloader() {
   return (
     <div class={styles.preloader}>
-      
+      <p class="utility">Truman Creative, LLC.</p>
     </div>
   )
 }
