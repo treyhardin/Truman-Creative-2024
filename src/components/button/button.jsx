8e18bb15176c2403c0ec5fae8af4490d0ec4b75d
Trey Hardin <treyhardindesign@gmail.com>
2023-11-18 20:23:47 -0500

    Project Button props fix

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,23 +1,21 @@
 import Icon from '../../utils/icons'
 import styles from './button.module.css'
 
-export default function Button({ text, url }) {
+export default function Button( props ) {
 
-  const handleClick = () => {
-    window.open(url, '_blank');
-  }
+  
 
   return (
-    <button  onClick={handleClick}>
+    <a class={styles.button} href={props.url} target='_blank'>
       <div class={styles.buttonText}>
         <Icon glyph="star" />
-        <p class="utility">{text}</p>
+        <p class="utility">{props.text}</p>
       </div>
       <div class={styles.buttonLines}>
         <span class={styles.buttonLine}></span>
         <span class={styles.buttonLine}></span>
         <span class={styles.buttonLine}></span>
       </div>
-    </button>
+    </a>
   )
 }
